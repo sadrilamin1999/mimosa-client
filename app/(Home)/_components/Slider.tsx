1eeead584685b6af290b695bcea9b40472d06b50
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-11-08 20:58:44 +0600

    project initilized

---
+++ app/(Home)/_components/Slider.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { data } from '@/data/sliderContents';
+import Image from 'next/image';
+import Overlay from '../../../components/ui/Overlay';
+import Link from 'next/link';
+import { cn } from '@/app/lib/utls';
+import { buttonVariants } from '@/components/ui/Button';
+
+const Slider = () => {
+  return (
+    <section className='h-[calc(100vh-5rem)] w-full'>
+      <Swiper
+        pagination={{ clickable: true }}
+        navigation={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        grabCursor={true}
+        speed={500}
+        modules={[Navigation, Pagination, Autoplay]}
+        className='mySwiper h-full w-full'
+      >
+        {data.map((slide) => (
+          <SwiperSlide key={slide.heading} className=' relative h-full w-full'>
+            <Image
+              src={slide.image}
+              alt={slide.heading}
+              width={1920}
+              height={1080}
+              className='h-full w-full object-cover'
+            />
+            <Overlay />
+            <div className='sp container absolute bottom-0 left-0 right-0 top-0 h-full w-full space-y-5 text-white'>
+              <h1>{slide.heading}</h1>
+              <p className=' max-w-6xl'>{slide.subHeading}</p>
+              <Link
+                href='/beauty-packages'
+                className={cn(
+                  buttonVariants({
+                    variant: 'secondary',
+                  })
+                )}
+              >
+                Brows Beauty Packages
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Slider;
